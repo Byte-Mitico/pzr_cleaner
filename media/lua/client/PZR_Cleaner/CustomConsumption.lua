381e89910e85f213e7562362c092ea6de6f7189c
MateusBMP <bmp.mateus@gmail.com>
2025-05-02 14:25:10 -0300

    Soap now during 100 more

---
+++ media/lua/client/PZR_Cleaner/CustomConsumption.lua
@@ -0,0 +1,10 @@
+local function adjustItem(Name, Property, Value)
+    local Item = ScriptManager.instance:getItem(Name)
+    Item:DoParam(Property .. " = " .. Value)
+end
+
+local function initCustomConsumption()
+    adjustItem("Base.Soap2", "UseDelta", 0.05 * 0.01)
+end
+
+Events.OnInitGlobalModData.Add(initCustomConsumption)
