20a322f1b30a7464d13b0048c515002967944a0e
MateusBMP <bmp.mateus@gmail.com>
2025-05-06 17:51:53 -0300

    ajusting the rebalance factor to 0.1

--- Contents/mods/PZR_Cleaner/media/lua/client/PZR_Cleaner/PZRA_CL_CustomConsumption.lua
+++ Contents/mods/PZR_Cleaner/media/lua/client/PZR_Cleaner/PZRA_CL_CustomConsumption.lua
@@ -4,7 +4,7 @@ local function adjustItem(Name, Property, Value)
 end
 
 local function initCustomConsumption()
-    adjustItem("Base.Soap2", "UseDelta", 0.05 * 0.01)
+    adjustItem("Base.Soap2", "UseDelta", 0.05 * 0.1)
 end
 
 Events.OnInitGlobalModData.Add(initCustomConsumption)
